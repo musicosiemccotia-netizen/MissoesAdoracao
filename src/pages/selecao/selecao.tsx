42dbd1603918ed11a1e5d4a5bb51f607695b382a
Daniel Oliveira Santos <musicos.iemc.cotia@gmail.com>
2026-07-04 23:03:55 -0300

    Descrição da alteração

--- src/pages/selecao/selecao.tsx
+++ src/pages/selecao/selecao.tsx
@@ -1,4 +1,5 @@
 import background from '../../assets/images/background/background.png'
+import logo from '../../assets/images/selecao/logo.png'
 
 import './selecao.css'
 
@@ -8,12 +9,28 @@ function Selecao() {
 
     <main className="selecao">
 
-      <div
-        className="selecao-background"
-        style={{ backgroundImage: `url(${background})` }}
-      ></div>
+  <div
+    className="selecao-background"
+    style={{ backgroundImage: `url(${background})` }}
+  ></div>
 
-    </main>
+  <div className="selecao-content">
+
+  <img
+    src={logo}
+    alt="Logo"
+    className="selecao-logo"
+  />
+
+  <p className="mensagem-semeando">
+    Ainda estamos semeando.
+    <br />
+    Se Deus permitir, logo, logo teremos frutos.
+  </p>
+
+</div>
+
+</main>
 
   )
 
